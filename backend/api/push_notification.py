71432e9366b5bd72c7e7cfb9b64a5ff3cddcf26f
blacklufus <michael.reischl@studmail.htw-aalen.de>
2025-11-17 23:08:37 +0100

    Update push_notification.py now secrets are stored in .env

--- backend/api/push_notification.py
+++ backend/api/push_notification.py
@@ -1,25 +1,26 @@
 import json
+import os
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("cred/serviceAccountKey.json")
+# with open("cred/serviceAccountKey.json") as f:
+#     print(json.dumps(f.read()))
+
+cred_json = os.getenv("FIREBASE_CREDENTIALS")
+
+if not cred_json:
+    raise RuntimeError("FIREBASE_CREDENTIALS not set")
+
+cred_dict = json.loads(cred_json)
+cred = credentials.Certificate(cred_dict)
 firebase_admin.initialize_app(cred)
 
 def getFcmToken():
-    filepath = "cred/fcmToken.txt"
 
-    with open(filepath, "r") as file:
-        FCM_TOKEN = file.read()
-
-    return FCM_TOKEN
+    return os.getenv("FCM_TOKEN")
 
 def getProjectId():
     return cred.project_id
 
 def getAccessToken():
     return cred.get_access_token().access_token
-
-
-
-# print(getProjectId())
-# print(getAccessToken())
