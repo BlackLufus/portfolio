eeb408d2e9514e858fbc8478c1878dee3f29aa74
blacklufus <michael.reischl@studmail.htw-aalen.de>
2025-11-24 13:41:54 +0100

    Fix an issue in project.gallery.overlay.tsx where multiple images pops when sliding to previous or next image.

--- frontend/app/components/project/project.gallery.overlay.tsx
+++ frontend/app/components/project/project.gallery.overlay.tsx
@@ -10,6 +10,12 @@ interface LightBoxOverlayProbs {
     onClose: () => void;
 }
 
+enum SliderState {
+    LEFT = -1,
+    NONE = 0,
+    RIGHT = 1
+}
+
 export default function LightBoxOverlay( {start, gallery, onClose}: LightBoxOverlayProbs) {
 
     const zoomImgRef = useRef<HTMLImageElement>(null);
@@ -18,45 +24,52 @@ export default function LightBoxOverlay( {start, gallery, onClose}: LightBoxOver
     const [translateX, setTranslateX] = useState(0);
     const [translateY, setTranslateY] = useState(0);
     const [scale, setScale] = useState(1);
-    const [slideState, setSlideState] = useState(0);
+    const [slideState, setSlideState] = useState(SliderState.NONE);
     const [touchStartX, setTouchStartX] = useState<number>(-1);
     const [touchDeltaX, setTouchDeltaX] = useState(0);
-    const [isSliding, setSlidingState] = useState(false);
 
+    // General dragging variables
+    const [isDragging, setDraggingState] = useState(false);
+    const lastCursorX = useRef(0);
+    const lastCursorY = useRef(0);
+    
+    // Variables for touches propose
+    const initialDistanceRef = useRef(0);
+    const initialScaleRef = useRef(1);
+    const lastMidpoint = useRef<Point>(new Point(0, 0));
+
+    // Variables for scale propose
     const SCALE_STEP = 0.13625;
     const MAX_SCALE = SCALE_STEP * 40;
     const MIN_SCALE = 1;
 
+    // Handle on left slide
     const onLeftClick = () => {
-        if (isSliding || index == 0) return;
+        if (slideState != SliderState.NONE || index == 0) return;
         if (scale != 1) {
             setScale(1);
         }
-        setSlideState(-1);
-        setSlidingState(true);
+        setSlideState(SliderState.LEFT);
         setTimeout(() => {
-            setSlideState(0);
+            setSlideState(SliderState.NONE);
             setIndex(index-1);
-            setSlidingState(false);
-        }, 345);
+        }, 350);
     }
 
+    // Handle on right slide
     const onRightClick = () => {
-        if (isSliding || index == gallery.length - 1) return;
+        if (slideState != SliderState.NONE || index == gallery.length - 1) return;
         if (scale != 1) {
             setScale(1);
         }
-        setSlideState(1);
-        setSlidingState(true);
+        setSlideState(SliderState.RIGHT);
         setTimeout(() => {
-            setSlideState(0);
+            setSlideState(SliderState.NONE);
             setIndex(index+1);
-            setSlidingState(false);
-        }, 345);
+        }, 350);
     }
 
-    const timeoutRef = useRef<number>(0);
-
+    // Handle on zoom
     const onZoom = (value: number) => {
         let newScale = scale + (value < 0 ? SCALE_STEP : -SCALE_STEP);
 
@@ -66,6 +79,10 @@ export default function LightBoxOverlay( {start, gallery, onClose}: LightBoxOver
         setScale(value == 0 ? 1 : newScale);
     }
 
+    // Reference for zoom animtation
+    const timeoutRef = useRef<number>(0);
+
+    // Animate zoom when button is pressed.
     const setZoomAnimation = () => {
         const img = zoomImgRef.current;
         if (!img) return;
@@ -78,17 +95,7 @@ export default function LightBoxOverlay( {start, gallery, onClose}: LightBoxOver
         }, 350);
     }
 
-    
-    const onMouseWheelMove = (e: WheelEvent) => {
-        e.stopPropagation();
-        onZoom(e.deltaY);
-    }
-
-
-    const initialDistanceRef = useRef(0);
-    const initialScaleRef = useRef(1);
-    const lastMidpoint = useRef<Point>(new Point(0, 0));
-
+    // Get distance between two touches
     const getDistance = (touches: TouchList) => {
         const [a, b] = [touches[0], touches[1]];
         const dx = a.clientX - b.clientX;
@@ -96,6 +103,7 @@ export default function LightBoxOverlay( {start, gallery, onClose}: LightBoxOver
         return Math.sqrt(dx*dx + dy*dy);
     };
 
+    // Calculate middle point between two touches
     const getMidpoint = (touches: TouchList) => {
         const x = (touches[0].clientX + touches[1].clientX) / 2;
         const y = (touches[0].clientY + touches[1].clientY) / 2;
@@ -104,10 +112,12 @@ export default function LightBoxOverlay( {start, gallery, onClose}: LightBoxOver
 
     const onTouchStart = (e: TouchEvent) => {
         e.preventDefault();
+
+        // How many touches are recognize on screen?
         if (e.touches.length === 1) {
             setTouchStartX(e.touches[0].clientX);
-            lastMouseX.current = e.touches[0].clientX;
-            lastMouseY.current = e.touches[0].clientY;
+            lastCursorX.current = e.touches[0].clientX;
+            lastCursorY.current = e.touches[0].clientY;
             setDraggingState(true);
         } else if (e.touches.length === 2) {
             initialDistanceRef.current = getDistance(e.touches);
@@ -118,29 +128,36 @@ export default function LightBoxOverlay( {start, gallery, onClose}: LightBoxOver
         }
     };
 
+    // Handle touch move
     const onTouchMove = (e: TouchEvent) => {
         e.preventDefault();
 
+        // How many touches are recognize on screen?
         if (e.touches.length === 1) {
 
             // When scale is 1 and 
-            if (scale == 1) {
+            if (scale == 1 && touchStartX != Infinity) {
+
+                // Get current x position
                 const clientX = e.touches[0].clientX;
                 setTouchDeltaX(clientX - touchStartX);
-                if (scale == 1) {
-                    const threshold = 75;
-                    if (touchDeltaX > threshold) {
-                        onLeftClick();
-                    } else if (touchDeltaX < -threshold) {
-                        onRightClick();
-                    }
+
+                // Check if distance is over threshold
+                const threshold = 75;
+                if (touchDeltaX > threshold) {
+                    onLeftClick();
+                    setTouchStartX(Infinity);
+                } else if (touchDeltaX < -threshold) {
+                    onRightClick();
+                    setTouchStartX(Infinity);
                 }
             }
-            const dx = e.touches[0].clientX - lastMouseX.current;
-            const dy = e.touches[0].clientY - lastMouseY.current;
+
+            const dx = e.touches[0].clientX - lastCursorX.current;
+            const dy = e.touches[0].clientY - lastCursorY.current;
             setTranslate(dx + translateX, dy + translateY);
-            lastMouseX.current = e.touches[0].clientX;
-            lastMouseY.current = e.touches[0].clientY;
+            lastCursorX.current = e.touches[0].clientX;
+            lastCursorY.current = e.touches[0].clientY;
         } else if (e.touches.length === 2) {
             const currentDistance = getDistance(e.touches);
             let newScale = initialScaleRef.current * (currentDistance / initialDistanceRef.current);
@@ -156,38 +173,44 @@ export default function LightBoxOverlay( {start, gallery, onClose}: LightBoxOver
         }
     };
 
+    // Handle touch end
     const onTouchEnd = (e: TouchEvent) => {
         if (e.touches.length === 0) setDraggingState(false);
         else if (e.touches.length === 1) {
-            lastMouseX.current = e.touches[0].clientX;
-            lastMouseY.current = e.touches[0].clientY;
+            lastCursorX.current = e.touches[0].clientX;
+            lastCursorY.current = e.touches[0].clientY;
         }
-        setTouchStartX(-1);
+        setTouchStartX(Infinity);
         setTouchDeltaX(0);
     };
 
-    const [isDragging, setDraggingState] = useState(false);
-    const lastMouseX = useRef(0);
-    const lastMouseY = useRef(0);
+    // Event on mouse wheel move
+    const onMouseWheelMove = (e: WheelEvent) => {
+        e.stopPropagation();
+        onZoom(e.deltaY);
+    }
 
+    // handle mouse down
     const onMouseDown = (e: MouseEvent) => {
         setDraggingState(true);
-        lastMouseX.current = e.clientX;
-        lastMouseY.current = e.clientY;
+        lastCursorX.current = e.clientX;
+        lastCursorY.current = e.clientY;
     };
     
+    // handle mouse move
     const onMouseMove = (e: MouseEvent) => {
         if (!isDragging) return;
 
-        const dx = e.clientX - lastMouseX.current;
-        const dy = e.clientY - lastMouseY.current;
+        const dx = e.clientX - lastCursorX.current;
+        const dy = e.clientY - lastCursorY.current;
 
         setTranslate(dx + translateX, dy + translateY)
 
-        lastMouseX.current = e.clientX;
-        lastMouseY.current = e.clientY;
+        lastCursorX.current = e.clientX;
+        lastCursorY.current = e.clientY;
     }
 
+    // set translate (x and y direction)
     const setTranslate = (x: number, y: number) => {
         const imgContainer = imageContainerRef.current;
         if (!imgContainer) return;
@@ -196,18 +219,12 @@ export default function LightBoxOverlay( {start, gallery, onClose}: LightBoxOver
         const maxWidth = imgContainer.clientWidth;
         const maxHeigh = imgContainer.clientHeight;
 
-        // console.log("maxWidth: ", maxWidth);
-        // console.log("maxHeigh: ", maxHeigh);
-
         const img = zoomImgRef.current;
         if (!img) return;
 
         // Get img width and height
         const actualWidth = img.getBoundingClientRect().width;
         const actualHeigth = img.getBoundingClientRect().height;
-
-        // console.log("actualWidth: ", actualWidth);
-        // console.log("actualHeigth: ", actualHeigth);
 
         const maxTranslateX = Math.max((actualWidth - maxWidth) / 2, 0);
         const maxTranslateY = Math.max((actualHeigth - maxHeigh) / 2, 0);
@@ -217,10 +234,12 @@ export default function LightBoxOverlay( {start, gallery, onClose}: LightBoxOver
         setTranslateY(Math.abs(y) < maxTranslateY ? y : y < 0 ? -maxTranslateY : maxTranslateY)
     }
 
+    // Handle on mouse up
     const onMouseUp = () => {
         setDraggingState(false);
     };
 
+    // Handle on key down
     const onKeydown = (e: KeyboardEvent) => {
         e.preventDefault();
         if (e.key == "ArrowLeft") {
@@ -274,11 +293,11 @@ export default function LightBoxOverlay( {start, gallery, onClose}: LightBoxOver
             <div className="light_box_overlay_content">
                 <div 
                     ref={imageContainerRef}
-                    className={`light_box_overlay_image_container ${slideState != 0 ? "slide" : ""}`}
+                    className={`light_box_overlay_image_container ${slideState != SliderState.NONE ? "slide" : ""}`}
                     style={
                             {
                                 "--start": "0", 
-                                "--stop": slideState == 1 ? "-100vw" : "100vw"
+                                "--stop": slideState == SliderState.LEFT ? "100vw" : "-100vw"
                             } as React.CSSProperties
                         }>
                     <img 
@@ -294,17 +313,18 @@ export default function LightBoxOverlay( {start, gallery, onClose}: LightBoxOver
                         src={gallery[index].url} 
                         alt="" />
                 </div>
-                { slideState != 0 && (<div 
+                { slideState != SliderState.NONE && (<div 
                     className="light_box_overlay_image_container slide"
                     style={
                             {
-                                "--start": slideState == -1 ? "-100vw" : "100vw", 
+                                // Move from 
+                                "--start": slideState == SliderState.LEFT ? "-100vw" : "100vw", 
                                 "--stop": "0"
                             } as React.CSSProperties
                         }>
                     <img 
                         className={`light_box_overlay_image ${isDragging ? "dragging" : ""}`}
-                        src={gallery[(slideState == 1 ? index+1 : index-1)].url} 
+                        src={gallery[(slideState == SliderState.LEFT ? index-1 : index+1)].url} 
                         alt="" />
                 </div>)}
             </div>
@@ -318,36 +338,36 @@ export default function LightBoxOverlay( {start, gallery, onClose}: LightBoxOver
                     <div
                         className="light_box_overlay_zoom" 
                         onClick={() => {
-                                setZoomAnimation();
-                                setTranslateX(0);
-                                setTranslateY(0);
-                                onZoom(1);
-                            }}>
-                            <svg width="30px" height="30px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M16 0c8.844 0 16 7.156 16 16s-7.156 16-16 16-16-7.156-16-16 7.156-16 16-16zM16 30.031c7.719 0 14-6.313 14-14.031s-6.281-14-14-14-14 6.281-14 14 6.281 14.031 14 14.031zM14.906 17h-5.906c-0.563 0-1-0.438-1-1s0.438-1 1-1h14c0.563 0 1 0.438 1 1s-0.438 1-1 1h-8.094z"></path>
-                            </svg>
+                            setZoomAnimation();
+                            setTranslateX(0);
+                            setTranslateY(0);
+                            onZoom(1);
+                        }}>
+                        <svg width="30px" height="30px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M16 0c8.844 0 16 7.156 16 16s-7.156 16-16 16-16-7.156-16-16 7.156-16 16-16zM16 30.031c7.719 0 14-6.313 14-14.031s-6.281-14-14-14-14 6.281-14 14 6.281 14.031 14 14.031zM14.906 17h-5.906c-0.563 0-1-0.438-1-1s0.438-1 1-1h14c0.563 0 1 0.438 1 1s-0.438 1-1 1h-8.094z"></path>
+                        </svg>
                     </div>
                     <div
                         className="light_box_overlay_zoom middle" 
                         onClick={() => {
-                                setZoomAnimation();
-                                setTranslateX(0);
-                                setTranslateY(0);
-                                onZoom(0);
-                            }}>
-                            <svg width="40px" height="40px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M22.154 2.554L16.707 8H21v1h-6V3h1v4.293l5.446-5.447zm-19.6 19.6L8 16.707V21h1v-6H3v1h4.293l-5.447 5.446zm19.6-.707L16.707 16H21v-1h-6v6h1v-4.293l5.446 5.446zM1.846 2.554L7.293 8H3v1h6V3H8v4.293L2.554 1.846z"/>
-                            </svg>
+                            setZoomAnimation();
+                            setTranslateX(0);
+                            setTranslateY(0);
+                            onZoom(0);
+                        }}>
+                        <svg width="40px" height="40px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M22.154 2.554L16.707 8H21v1h-6V3h1v4.293l5.446-5.447zm-19.6 19.6L8 16.707V21h1v-6H3v1h4.293l-5.447 5.446zm19.6-.707L16.707 16H21v-1h-6v6h1v-4.293l5.446 5.446zM1.846 2.554L7.293 8H3v1h6V3H8v4.293L2.554 1.846z"/>
+                        </svg>
                     </div>
                     <div
                         className="light_box_overlay_zoom" 
                         onClick={() => {
-                                setZoomAnimation();
-                                onZoom(-1);
-                            }}>
-                            <svg width="30px" height="30px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M16 0c-8.836 0-16 7.163-16 16s7.163 16 16 16c8.837 0 16-7.163 16-16s-7.163-16-16-16zM16 30.032c-7.72 0-14-6.312-14-14.032s6.28-14 14-14 14 6.28 14 14-6.28 14.032-14 14.032zM23 15h-6v-6c0-0.552-0.448-1-1-1s-1 0.448-1 1v6h-6c-0.552 0-1 0.448-1 1s0.448 1 1 1h6v6c0 0.552 0.448 1 1 1s1-0.448 1-1v-6h6c0.552 0 1-0.448 1-1s-0.448-1-1-1z"></path>
-                            </svg>
+                            setZoomAnimation();
+                            onZoom(-1);
+                        }}>
+                        <svg width="30px" height="30px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M16 0c-8.836 0-16 7.163-16 16s7.163 16 16 16c8.837 0 16-7.163 16-16s-7.163-16-16-16zM16 30.032c-7.72 0-14-6.312-14-14.032s6.28-14 14-14 14 6.28 14 14-6.28 14.032-14 14.032zM23 15h-6v-6c0-0.552-0.448-1-1-1s-1 0.448-1 1v6h-6c-0.552 0-1 0.448-1 1s0.448 1 1 1h6v6c0 0.552 0.448 1 1 1s1-0.448 1-1v-6h6c0.552 0 1-0.448 1-1s-0.448-1-1-1z"></path>
+                        </svg>
                     </div>
                 </div>
                 <div 
